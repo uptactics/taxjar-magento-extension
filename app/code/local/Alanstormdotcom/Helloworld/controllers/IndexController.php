35ac23d590b13f9e8c13155c120a06e883866fa2
Bernd Ustorf <bernd.ustorf@gmail.com>
2014-03-21 16:45:14 -0700

    demo extension

---
+++ app/code/local/Alanstormdotcom/Helloworld/controllers/IndexController.php
@@ -0,0 +1,8 @@
+<?php
+
+class Alanstormdotcom_Helloworld_IndexController extends Mage_Core_Controller_Front_Action {        
+    public function indexAction() {
+        echo 'Hello Index!';
+    }
+}
+
