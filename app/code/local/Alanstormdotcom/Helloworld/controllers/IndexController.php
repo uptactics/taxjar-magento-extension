6e7290f235dbcb8a5a7b84bba345536e08a4d67f
Bernd Ustorf <bernd.ustorf@gmail.com>
2014-03-22 08:33:10 -0700

    get rid of old demo

--- app/code/local/Alanstormdotcom/Helloworld/controllers/IndexController.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Alanstormdotcom_Helloworld_IndexController extends Mage_Core_Controller_Front_Action {        
-    public function indexAction() {
-        echo 'Hello Index!';
-    }
-}
-
