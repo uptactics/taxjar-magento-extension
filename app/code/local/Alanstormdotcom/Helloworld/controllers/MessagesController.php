c17db9c1f747d6727d5e72784e07baa8c6b5367e
Bernd Ustorf <bernd.ustorf@gmail.com>
2014-03-22 09:19:44 -0700

    get rid of old tutorial

--- app/code/local/Alanstormdotcom/Helloworld/controllers/MessagesController.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Alanstormdotcom_Helloworld_MessagesController extends Mage_Core_Controller_Front_Action
-{
-	public function goodbyeAction()         
-	{
-		echo 'Another Goodbye';
-	}
-}
