9dcc1aa47af9c73ca4621a96c328b5cc2356012d
Bernd Ustorf <bernd.ustorf@gmail.com>
2014-03-21 17:01:33 -0700

    taxjar extension

---
+++ app/code/local/Taxjar/Rateupdater/controllers/IndexController.php
@@ -0,0 +1,8 @@
+<?php
+
+class Taxjar_Rateupdater_IndexController extends Mage_Core_Controller_Front_Action {        
+    public function indexAction() {
+        echo "Let's update some friggin' rates!";
+    }
+}
+
