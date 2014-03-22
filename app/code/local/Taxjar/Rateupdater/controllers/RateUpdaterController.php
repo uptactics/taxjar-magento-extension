17117ccddd8c68500fd1009391395c1bb93692ac
Bernd Ustorf <bernd.ustorf@gmail.com>
2014-03-22 09:40:17 -0700

    convert to admin extension

--- app/code/local/Taxjar/Rateupdater/controllers/RateUpdaterController.php
+++ app/code/local/Taxjar/Rateupdater/controllers/RateUpdaterController.php
@@ -1,6 +1,6 @@
 <?php
 
-class Taxjar_Rateupdater_IndexController extends Mage_Core_Controller_Front_Action {        
+class Taxjar_Rateupdater_RateUpdaterController extends Mage_Core_Controller_Front_Action {        
     public function indexAction() {
         echo "Let's update some friggin' rates!";
         $rateModel = Mage::getModel('tax/calculation_rate');
