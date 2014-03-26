55979094327a2ab6603193e43a187811ffcaaff8
Bernd Ustorf <bernd.ustorf@gmail.com>
2014-03-25 18:42:25 -0700

    wip.  connected to external api in observer

--- app/code/local/Taxjar/Rateupdater/Model/Observer.php
+++ app/code/local/Taxjar/Rateupdater/Model/Observer.php
@@ -6,8 +6,21 @@ class Taxjar_Rateupdater_Model_Observer {
         //You can use this data to manipulate the order data before it's saved.
         //Uncomment the line below to log what is contained here:
         //Mage::log($observer);
-
-        Mage::log('We just made an Observer!');
+        $regionId = Mage::getStoreConfig('shipping/origin/region_id');        
+        $regionCode = Mage::getModel('directory/region')->load($regionId)->getCode();
+        Mage::log($regionCode);
+        $client = new Varien_Http_Client('http://localhost:4000/magento/get_configuration/TX');
+        $client->setMethod(Varien_Http_Client::GET);
+        $client->setHeaders('Authorization', 'Token token="49452ec7f67ea6fa88fb05492f02443c"');
+        //more parameters
+        try{
+            $response = $client->request();
+            if ($response->isSuccessful()) {
+              Mage::log($response);
+            }
+        } catch (Exception $e) {
+          Mage::log($e);
+        }        
     }
 
 }
