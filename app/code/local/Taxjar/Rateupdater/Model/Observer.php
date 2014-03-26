3b9d1cbc328d7de7a109832058d70c4ebe7d6562
Bernd Ustorf <bernd.ustorf@gmail.com>
2014-03-26 10:32:03 -0700

    refactor observer

--- app/code/local/Taxjar/Rateupdater/Model/Observer.php
+++ app/code/local/Taxjar/Rateupdater/Model/Observer.php
@@ -1,27 +1,42 @@
 <?php
 class Taxjar_Rateupdater_Model_Observer {
 
-    public function logobs($observer) {
-        //$observer contains data passed from when the event was triggered.
-        //You can use this data to manipulate the order data before it's saved.
-        //Uncomment the line below to log what is contained here:
-        //Mage::log($observer);
-        $regionId = Mage::getStoreConfig('shipping/origin/region_id');        
-        $regionCode = Mage::getModel('directory/region')->load($regionId)->getCode();
-        Mage::log($regionCode);
-        $client = new Varien_Http_Client('http://localhost:4000/magento/get_configuration/TX');
-        $client->setMethod(Varien_Http_Client::GET);
-        $client->setHeaders('Authorization', 'Token token="49452ec7f67ea6fa88fb05492f02443c"');
-        //more parameters
-        try{
-            $response = $client->request();
-            if ($response->isSuccessful()) {
-              Mage::log($response);
-            }
-        } catch (Exception $e) {
-          Mage::log($e);
-        }        
+  public function execute($observer) {
+    Mage::log($this->getConfiguration()->getBody());
+  }
+
+  private function getApiKey() {
+    return Mage::getStoreConfig('rateupdater_options/states/rateupdater_apikey');
+  }
+
+  private function getRegionCode() {
+    $regionId = Mage::getStoreConfig('shipping/origin/region_id');
+    return Mage::getModel('directory/region')->load($regionId)->getCode();
+  }
+
+  private function getConfigUrl() {
+    return 'http://localhost:4000/magento/get_configuration/' . $this->getRegionCode();
+  }
+
+  private function getAuthHeader() {
+    return 'Token token="' . $this->getApiKey() .  '"';
+  }
+
+  private function getClient($url) {
+    $client = new Varien_Http_Client($url);
+    $client->setMethod(Varien_Http_Client::GET);
+    $client->setHeaders('Authorization', $this->getAuthHeader());
+    return $client;
+  }
+
+  private function getConfiguration() {
+    $response = $this->getClient($this->getConfigUrl())->request();
+    if ($response->isSuccessful()) {
+      return $response;
+    } else {
+      throw new Exception('Could not connect to TaxJar.');
     }
+  }
 
 }
 ?>
