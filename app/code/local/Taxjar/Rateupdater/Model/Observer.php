10f69d7ec0bb4488f47be7022e8e84f4b325f067
Bernd Ustorf <bernd.ustorf@gmail.com>
2014-03-26 13:01:42 -0700

    set config based on api response

--- app/code/local/Taxjar/Rateupdater/Model/Observer.php
+++ app/code/local/Taxjar/Rateupdater/Model/Observer.php
@@ -2,37 +2,50 @@
 class Taxjar_Rateupdater_Model_Observer {
 
   public function execute($observer) {
-    Mage::log($this->getConfiguration()->getBody());
+    $configJson = $this->_getConfiguration();
+    $this->_setTaxBasis($configJson);
+    $this->_setShippingTaxability($configJson);
   }
 
-  private function getApiKey() {
-    return Mage::getStoreConfig('rateupdater_options/states/rateupdater_apikey');
+  // private methods
+  
+  private function _setShippingTaxability($configJson) {
+    $taxClass = 0;
+    if($configJson['shipping_taxable']) {
+      $taxClass = 4;
+    }
+    $this->_setConfig('tax/classes/shipping_tax_class', $taxClass);
   }
 
-  private function getRegionCode() {
-    $regionId = Mage::getStoreConfig('shipping/origin/region_id');
-    return Mage::getModel('directory/region')->load($regionId)->getCode();
+  private function _setTaxBasis($configJson) {
+    $basis = 'shipping';
+    if($configJson['origin_based']) {
+      $basis = 'origin';
+    }
+    $this->_setConfig('tax/calculation/based_on', $basis);
   }
 
-  private function getConfigUrl() {
-    return 'http://localhost:4000/magento/get_configuration/' . $this->getRegionCode();
+  private function _setConfig($path, $value){
+    $config = new Mage_Core_Model_Config();
+    $config->saveConfig($path, $value, 'default', 0);
   }
 
-  private function getAuthHeader() {
-    return 'Token token="' . $this->getApiKey() .  '"';
-  }
-
-  private function getClient($url) {
+  private function _getClient($url) {
+    $apiKey = Mage::getStoreConfig('rateupdater_options/states/rateupdater_apikey');
     $client = new Varien_Http_Client($url);
     $client->setMethod(Varien_Http_Client::GET);
-    $client->setHeaders('Authorization', $this->getAuthHeader());
+    $client->setHeaders('Authorization', 'Token token="' . $apiKey .  '"');
     return $client;
   }
 
-  private function getConfiguration() {
-    $response = $this->getClient($this->getConfigUrl())->request();
+  private function _getConfiguration() {
+    $regionId   = Mage::getStoreConfig('shipping/origin/region_id');
+    $regionCode = Mage::getModel('directory/region')->load($regionId)->getCode();
+    $url        = 'http://localhost:4000/magento/get_configuration/' . $regionCode;
+    $response   = $this->_getClient($url)->request();
     if ($response->isSuccessful()) {
-      return $response;
+      $json = $response->getBody();      
+      return json_decode($json, true);
     } else {
       throw new Exception('Could not connect to TaxJar.');
     }
