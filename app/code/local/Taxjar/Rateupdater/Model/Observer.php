4df19c0c44064d18f2e11f08af7b9dfa1df09700
Bernd Ustorf <bernd.ustorf@gmail.com>
2014-03-26 17:55:43 -0700

    create rates and associate with rule

--- app/code/local/Taxjar/Rateupdater/Model/Observer.php
+++ app/code/local/Taxjar/Rateupdater/Model/Observer.php
@@ -7,13 +7,19 @@ class Taxjar_Rateupdater_Model_Observer {
     $configJson = $this->_getResource('configuration', $regionCode);
     $this->_setTaxBasis($configJson);
     $this->_setShippingTaxability($configJson);
-    $this->_purgeExisting('tax/calculation_rule');
+    $this->_purgeExisting('tax/calculation');
     $this->_purgeExisting('tax/calculation_rate');
-    $rateJson = $this->_getResource('rates', $regionCode);
-    Mage::log($rateJson);
+    $this->_createRates($regionCode);
   }
 
   // private methods
+
+  private function _createRates($regionCode) {
+    $ratesJson = $this->_getResource('rates', $regionCode);
+    foreach($ratesJson as $rateJson) {
+      $this->_createRate($rateJson);
+    }    
+  }
 
   private function _createRate($rateJson) {
     $rateModel = Mage::getModel('tax/calculation_rate');
@@ -23,10 +29,20 @@ class Taxjar_Rateupdater_Model_Observer {
     $rateModel->setCode('US-CA-walnut-creek-Rate 1');
     $rateModel->setRate('8.2500');
     $rateModel->save();
+    $rateId = $rateModel->getId();
+
+    $taxCalculationData = array(
+      'tax_calculation_rate_id'   => $rateId,
+      'tax_calculation_rule_id'   => 1,
+      'customer_tax_class_id'     => 3,
+      'product_tax_class_id'      => 2
+    );
+
+    Mage::getSingleton('tax/calculation')->setData($taxCalculationData)->save();
   }
 
   private function _purgeExisting($path) {
-    $existingRecords = Mage::getModel($path)->getCollection();
+    $existingRecords = Mage::getModel($path)->getCollection();    
     foreach($existingRecords as $record) {
       $record->delete();
     }        
