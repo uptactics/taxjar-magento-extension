e821279e724fea19f7a45d4fceaac2e4364adfb6
Bernd Ustorf <bernd.ustorf@gmail.com>
2014-03-28 14:25:29 -0700

    blow away and recreate all rules and rates, every time

--- app/code/local/Taxjar/Salestaxwizard/Model/Observer.php
+++ app/code/local/Taxjar/Salestaxwizard/Model/Observer.php
@@ -7,16 +7,17 @@ class Taxjar_Salestaxwizard_Model_Observer {
     $this->configuration->setShippingTaxability($this->configJson);
     $this->_purgeExisting();
     $this->_createRates($this->regionCode);
-    $this->shippingrule->createIfTaxable($this->configJson, $this->newRates);
+    $this->rule->create('Retail Customer-Taxable Goods-Rate 1', 2, 1, $this->newRates);
+    if($this->configJson['shipping_taxable']) {
+      $this->rule->create('Retail Customer-Shipping-Rate 1', 4, 2, $this->newRates);
+    }
   }
-
-
 
   private function _setVariables() {
     $this->newRates      = array();
     $this->client        = Mage::getModel('salestaxwizard/client');
     $this->configuration = Mage::getModel('salestaxwizard/configuration');
-    $this->shippingrule  = Mage::getModel('salestaxwizard/shippingrule');
+    $this->rule          = Mage::getModel('salestaxwizard/rule');
     $this->rate          = Mage::getModel('salestaxwizard/rate');
     $regionId            = Mage::getStoreConfig('shipping/origin/region_id');
     $this->regionCode    = Mage::getModel('directory/region')->load($regionId)->getCode();
@@ -24,7 +25,7 @@ class Taxjar_Salestaxwizard_Model_Observer {
   }
 
   private function _purgeExisting() {
-    $paths = array('tax/calculation', 'tax/calculation_rate');
+    $paths = array('tax/calculation', 'tax/calculation_rate', 'tax/calculation_rule');
     foreach($paths as $path){
       $existingRecords = Mage::getModel($path)->getCollection();    
       foreach($existingRecords as $record) {
--- app/code/local/Taxjar/Salestaxwizard/Model/Rate.php
+++ app/code/local/Taxjar/Salestaxwizard/Model/Rate.php
@@ -11,25 +11,8 @@ class Taxjar_Salestaxwizard_Model_Rate {
     $rateModel->setRate('8.2500');
     $rateModel->save();
 
-    $rateId = $rateModel->getId();
-    
-    $this->createTaxCalculation($rateId);
-
-    return $rateId;
+    return $rateModel->getId();
   }
-
-  private function createTaxCalculation($rateId) {
-
-    $taxCalculationData = array(
-      'tax_calculation_rate_id'   => $rateId,
-      'tax_calculation_rule_id'   => 1,
-      'customer_tax_class_id'     => 3,
-      'product_tax_class_id'      => 2
-    );
-
-    Mage::getSingleton('tax/calculation')->setData($taxCalculationData)->save();
-
-  }
-
+  
 }
 ?>
