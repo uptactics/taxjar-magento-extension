7b0533e9caf35a47087c033a204d4f4bf05fa5e6
Bernd Ustorf <bernd.ustorf@gmail.com>
2014-03-28 14:54:35 -0700

    refactor

--- app/code/local/Taxjar/Salestaxwizard/Model/Observer.php
+++ app/code/local/Taxjar/Salestaxwizard/Model/Observer.php
@@ -2,27 +2,37 @@
 class Taxjar_Salestaxwizard_Model_Observer {
   
   public function execute($observer) {
-    $this->_setVariables();   
-    $this->configuration->setTaxBasis($this->configJson);
-    $this->configuration->setShippingTaxability($this->configJson);
+    
+    $this->newRates = array();
+
+    $client         = Mage::getModel('salestaxwizard/client');
+    $configuration  = Mage::getModel('salestaxwizard/configuration');
+    $rule           = Mage::getModel('salestaxwizard/rule');
+    $regionId       = Mage::getStoreConfig('shipping/origin/region_id');
+    $regionCode     = Mage::getModel('directory/region')->load($regionId)->getCode();
+
+    $configJson     = $client->getResource('configuration', $regionCode);
+    $ratesJson      = $client->getResource('rates', $regionCode);
+
+    $configuration->setTaxBasis($configJson);
+
+    $configuration->setShippingTaxability($configJson);
+
     $this->_purgeExisting();
-    $this->_createRates($this->regionCode);
-    $this->rule->create('Retail Customer-Taxable Goods-Rate 1', 2, 1, $this->newRates);
-    if($this->configJson['shipping_taxable']) {
-      $this->rule->create('Retail Customer-Shipping-Rate 1', 4, 2, $this->newRates);
+
+    $this->_createRates($ratesJson);
+
+    $rule->create('Retail Customer-Taxable Goods-Rate 1', 2, 1, $this->newRates);
+
+    if($configJson['shipping_taxable']) {
+
+      $rule->create('Retail Customer-Shipping-Rate 1', 4, 2, $this->newRates);
+
     }
+
   }
 
-  private function _setVariables() {
-    $this->newRates      = array();
-    $this->client        = Mage::getModel('salestaxwizard/client');
-    $this->configuration = Mage::getModel('salestaxwizard/configuration');
-    $this->rule          = Mage::getModel('salestaxwizard/rule');
-    $this->rate          = Mage::getModel('salestaxwizard/rate');
-    $regionId            = Mage::getStoreConfig('shipping/origin/region_id');
-    $this->regionCode    = Mage::getModel('directory/region')->load($regionId)->getCode();
-    $this->configJson    = $this->client->getResource('configuration', $this->regionCode);    
-  }
+
 
   private function _purgeExisting() {
     $paths = array('tax/calculation', 'tax/calculation_rate', 'tax/calculation_rule');
@@ -34,10 +44,10 @@ class Taxjar_Salestaxwizard_Model_Observer {
     }
   }
 
-  private function _createRates($regionCode) {
-    $ratesJson = $this->client->getResource('rates', $regionCode);
+  private function _createRates($ratesJson) {
+    $rate = Mage::getModel('salestaxwizard/rate');
     foreach($ratesJson as $rateJson) {
-      $this->newRates[] = $this->rate->create($rateJson);
+      $this->newRates[] = $rate->create($rateJson);
     }    
   }
 
