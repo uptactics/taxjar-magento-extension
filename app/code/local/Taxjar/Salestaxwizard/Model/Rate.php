26da7f2cef7f78b38c8d52af915f200084528f3e
Bernd Ustorf <bernd.ustorf@gmail.com>
2014-04-01 11:48:55 -0700

    use values from ratesJson

--- app/code/local/Taxjar/Salestaxwizard/Model/Rate.php
+++ app/code/local/Taxjar/Salestaxwizard/Model/Rate.php
@@ -1,14 +1,15 @@
 <?php
 class Taxjar_Salestaxwizard_Model_Rate {
 
-  public function create($rateJson) {
-
+  public function create($rateJson) {    
+    $zip       = $rateJson['zip'];
+    $rate      = $rateJson['rate'];
     $rateModel = Mage::getModel('tax/calculation_rate');
     $rateModel->setTaxCountryId('US');
     $rateModel->setTaxRegionId(12);
-    $rateModel->setTaxPostcode('94597');
-    $rateModel->setCode('US-CA-walnut-creek-Rate 1');
-    $rateModel->setRate('8.2500');
+    $rateModel->setTaxPostcode($zip);
+    $rateModel->setCode('US-CA-' . $zip);
+    $rateModel->setRate($rate);
     $rateModel->save();
 
     return $rateModel->getId();
