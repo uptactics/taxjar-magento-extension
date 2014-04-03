ff901bdb19b2b071f2c5d37a5fead0242742baf4
Bernd Ustorf <bernd.ustorf@gmail.com>
2014-04-03 12:39:34 -0700

    create api settings when installing extension

--- app/code/community/Taxjar/Salestaxwizard/Model/Observer.php
+++ app/code/community/Taxjar/Salestaxwizard/Model/Observer.php
@@ -4,46 +4,32 @@ class Taxjar_Salestaxwizard_Model_Observer {
   public function execute($observer) {
     
     $this->newRates = array();
-
     $client         = Mage::getModel('salestaxwizard/client');
     $configuration  = Mage::getModel('salestaxwizard/configuration');
     $rule           = Mage::getModel('salestaxwizard/rule');
     $regionId       = Mage::getStoreConfig('shipping/origin/region_id');
     $regionCode     = Mage::getModel('directory/region')->load($regionId)->getCode();
     $storeZip       = Mage::getStoreConfig('shipping/origin/postcode');
-
     $apiHost = 'http://76.21.49.40:4000/';
-      
     $configJson = $client->getResource(
       $apiHost . '/magento/get_configuration/' . $regionCode
     );
-
     if(!$configJson['allow_update']) {      
       return;
     }
-
     $ratesJson = $client->getResource(
       $apiHost . '/magento/get_rates/' . $regionCode . '/' . $storeZip
     );
-
     $configuration->setTaxBasis($configJson);
-
     $configuration->setShippingTaxability($configJson);
-
     $configuration->setDisplaySettings();
-
+    $configuration->setApiSettings();
     $this->_purgeExisting();
-
     $this->_createRates($ratesJson);
-
     $rule->create('Retail Customer-Taxable Goods-Rate 1', 2, 1, $this->newRates);
-
     if($configJson['freight_taxable']) {
-
       $rule->create('Retail Customer-Shipping-Rate 1', 4, 2, $this->newRates);
-
     }
-
   }
 
 
