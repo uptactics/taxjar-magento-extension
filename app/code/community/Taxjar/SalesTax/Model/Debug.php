c834b4ec06ba33ea8f6cb83f74ad8da26041c63b
Jake Johnson <jake@fastdivision.com>
2016-02-01 10:43:04 -0800

    Version 1.5.0

--- app/code/community/Taxjar/SalesTax/Model/Debug.php
+++ app/code/community/Taxjar/SalesTax/Model/Debug.php
@@ -33,7 +33,7 @@ class Taxjar_SalesTax_Model_Debug {
     $states         = implode(',', unserialize(Mage::getStoreConfig('taxjar/config/states')));
     $apiUser        = Mage::getModel('api/user');
     $existingUserId = $apiUser->load('taxjar', 'username')->getUserId();
-    $pluginVersion  = '1.4.4';
+    $pluginVersion  = '1.5.0';
     $phpMemory      = @ini_get('memory_limit');
     $phpVersion     = @phpversion();
     $magentoVersion = Mage::getVersion();
