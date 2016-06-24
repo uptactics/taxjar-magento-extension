28892f9a5ae35971d68ed83335b0d9e6384ca5bb
Jake Johnson <jake@fastdivision.com>
2016-06-24 11:50:55 -0700

    Fall back to Magento's native `collect` method for CE 1.7 support

--- app/code/community/Taxjar/SalesTax/Model/Sales/Total/Quote/Tax.php
+++ app/code/community/Taxjar/SalesTax/Model/Sales/Total/Quote/Tax.php
@@ -74,60 +74,7 @@ class Taxjar_SalesTax_Model_Sales_Total_Quote_Tax extends Mage_Tax_Model_Sales_T
                 }    
             }
         } else {
-            $this->_store = $address->getQuote()->getStore();
-            $customer = $address->getQuote()->getCustomer();
-            if ($customer) {
-                $this->_calculator->setCustomer($customer);
-            }
-
-            if (!$address->getAppliedTaxesReset()) {
-                $address->setAppliedTaxes(array());
-            }
-
-            $items = $this->_getAddressItems($address);
-            if (!count($items)) {
-                return $this;
-            }
-            $request = $this->_calculator->getRateRequest(
-                $address,
-                $address->getQuote()->getBillingAddress(),
-                $address->getQuote()->getCustomerTaxClassId(),
-                $this->_store
-            );
-
-            if ($this->_config->priceIncludesTax($this->_store)) {
-                if ($this->_helper->isCrossBorderTradeEnabled($this->_store)) {
-                    $this->_areTaxRequestsSimilar = true;
-                } else {
-                    $this->_areTaxRequestsSimilar = $this->_calculator->compareRequests(
-                        $this->_calculator->getRateOriginRequest($this->_store),
-                        $request
-                    );
-                }
-            }
-
-            switch ($this->_config->getAlgorithm($this->_store)) {
-                case Mage_Tax_Model_Calculation::CALC_UNIT_BASE:
-                    $this->_unitBaseCalculation($address, $request);
-                    break;
-                case Mage_Tax_Model_Calculation::CALC_ROW_BASE:
-                    $this->_rowBaseCalculation($address, $request);
-                    break;
-                case Mage_Tax_Model_Calculation::CALC_TOTAL_BASE:
-                    $this->_totalBaseCalculation($address, $request);
-                    break;
-                default:
-                    break;
-            }
-
-            $this->_addAmount($address->getExtraTaxAmount());
-            $this->_addBaseAmount($address->getBaseExtraTaxAmount());
-            $this->_calculateShippingTax($address, $request);
-
-            $this->_processHiddenTaxes();
-
-            //round total amounts in address
-            $this->_roundTotals($address);
+            return parent::collect($address);
         }
 
         return $this;
